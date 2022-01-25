34276e27ec1588b73a41ae34c0cb1518aa79ddf3
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-01-25 20:26:19 -0300

    first commit

---
+++ src/main/kotlin/br/com/larassistencialmatilde/sgs/SistemaDeGestaoSocialApplication.kt
@@ -0,0 +1,11 @@
+package br.com.larassistencialmatilde.sgs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemaDeGestaoSocialApplication
+
+fun main(args: Array<String>) {
+	runApplication<SistemaDeGestaoSocialApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/larassistencialmatilde/sgs/SistemaDeGestaoSocialApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.larassistencialmatilde.sgs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaDeGestaoSocialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
